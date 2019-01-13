ca4a1a9f1de8b13ad7c9c685615fa6516a3c95b4
huskyhoochu <dfg1499@gmail.com>
2019-01-13 21:26:11 +0900

    [#1] database 관련 파일 추가
    - convert.go: json 파일을 csv 파일로 변경
    - product.csv: 상품 데이터에 대한 csv 파일
    - database.db: sqlite3 파일

---
+++ convert.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+type Product struct {
+	ImageUrl  string `json:"image_url"`
+	MobileUrl string `json:"mobile_url"`
+	Price     int    `json:"price"`
+	ShopEn    string `json:"shop_en"`
+	ShopKo    string `json:"shop_ko"`
+	Title     string `json:"title"`
+	Url       string `json:"url"`
+}
+
+func Convert() {
+	raw, err := ioutil.ReadFile("./product.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var jsonData []Product
+	err = json.Unmarshal([]byte(raw), &jsonData)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	csvData, err := os.Create("./product.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvData.Close()
+
+	writer := csv.NewWriter(csvData)
+
+	for _, worker := range jsonData {
+		var record []string
+		record = append(record, worker.ImageUrl)
+		record = append(record, worker.MobileUrl)
+		record = append(record, strconv.Itoa(worker.Price))
+		record = append(record, worker.ShopEn)
+		record = append(record, worker.ShopKo)
+		record = append(record, worker.Title)
+		record = append(record, worker.Url)
+		writer.Write(record)
+	}
+	writer.Flush()
+}
